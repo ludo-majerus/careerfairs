4e686f0b3a74aaea159382cba394dd788b0bdf81
Ludo <ludovic.majerus@careerbuilder.com>
2012-10-26 14:32:44 +0200

    Push on github

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Careerfairs::Application.config.secret_token = '737dcfd671745bf7568425dda288b9da79429d614a9aa75d4daf528c1eb51ea496167f5d4d77c2486831ca7dd2bb355427ec6e420c7c2f2978d910a79ec21935'
