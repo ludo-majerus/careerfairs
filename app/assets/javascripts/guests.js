2dd2267e9bbe79de21a5e44a5913b06af31f83be
rehrenfeld <romain.ehrenfeld@careerbuilder.com>
2013-01-04 12:07:04 +0100

    send

--- app/assets/javascripts/guests.js
+++ app/assets/javascripts/guests.js
@@ -1,1 +1,11 @@
-// Romain E.
+$(document).ready(function() {
+	$("#guest_event_id").change(function() {
+	    idEvent = $("#guest_event_id").val();
+	    var data = null;
+	    $.getJSON('/events/'+idEvent+'.json',function(data)
+	    { 
+	      $("#eventName").fadeToggle("slow", "linear");    
+	      $("#eventName").html(data.event_name); 
+	    })  
+    })
+})
