3144208607337894ed25bc1a9bb5148265cd720c
ludo-majerus <ludovic.majerus@careerbuilder.com>
2013-02-22 15:06:46 +0100

    trial for fixing the .gitignore | remove breadcrumb, need fix or I'll completely remove it

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,5 +3,5 @@ class Job < ActiveRecord::Base
 	has_one :company, :through => :companytoevent
 	has_one :event, :through => :companytoevent
 	attr_accessible :companytoevent_id, :title, :category, :experience
-	validates :companytoevent_id, :presence => true
+	validates :companytoevent_id, :title, :category, :experience, :presence => true
 end
